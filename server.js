c6fa1bc8631b0c8974a02cc14cf32e34c9130061
Nat Sharpe <sharpe1890@gmail.com>
2018-10-03 22:45:03 -0400

    Checks scan POST and responds true or false

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 // const db = require('./db');
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/scan', (req, res) => {
+  let item = req.body;
+  let response = {
+    status: (item.id > 4) ? false : true
+  };
+  res.send(JSON.stringify(response));
+});
+
 
 app.get('/', (req, res) => {
  res.end('home page');
