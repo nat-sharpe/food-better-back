0cf8cf9fa402de309ced7f7cf6784db75213eec7
Nat Sharpe <sharpe1890@gmail.com>
2018-10-08 23:28:46 -0400

    Server responds with correct id and item data

--- server.js
+++ server.js
@@ -10,14 +10,50 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const dummyDatabase = [
   {
-    id: 10,
+    id: '1',
     allowed: true,
-    name: 'Low Fat Peanut Butter',
-    brand: 'JIF',
-    imageURL: 'https://images.freshop.com/00051500240908/5df3822b3c65c7aa03f12e2910385084_large.png',
-    carbs: 10,
-    calories: 20,
+    name: 'Creamy Peanut Butter',
+    brand: 'Peter Pan',
+    imageURL: 'https://lh3.googleusercontent.com/proxy/YwCPZgrjfS4RoNX7Mqkt2GjFIPNNrTxL6tiXA2imSIOQKPbiEWai_13CnH8ifNvijCOYdKVX7sg94hgsQdTg_I4_oEP0oFg9VmWaSa4alQfwN23WdVJDRVi1d6I7z8Yj4JZhVxY_XGyVmdqNveVByVj3J7EhjByJiLtxI-B1h23G2L4zYg=s500-pd-e365-rw-pc0xffffff',
+    carbs: 16,
+    calories: 220,
     organic: false,
+    vegan: false,
+    glutenFree: true,
+  },
+  {
+    id: '2',
+    allowed: true,
+    name: 'Reduced Fat Crunchy Peanut Butter',
+    brand: 'Jif',
+    imageURL: 'https://lh3.googleusercontent.com/proxy/UQUtrqFlB2Z51RPSwBrDS_vnAf_kzyOVYpxpN2CICpyvJ_lIrJpEvEXWV41LCYdjEQWnOBZVKUs_V-KiEu_fFLh7vPYAcTNtFwt_Wp1QgEYEzHjvOAlp93z6GniIRtHqW3nq9ArjovTsLIoNAauEs0xy8muUqRGoLbjd6-4jjoasDKtBj6Y=s500-pd-e365-rw-pc0xffffff',
+    carbs: 6,
+    calories: 70,
+    organic: false,
+    vegan: true,
+    glutenFree: true,
+  },
+  {
+    id: '20',
+    allowed: true,
+    name: 'Reduced Fat Creamy Peanut Butter',
+    brand: 'Skippy',
+    imageURL: 'https://lh3.googleusercontent.com/proxy/hfUR8oi-1wHoRLE68SNb94IpAnqF5OUx4Ho5lBPXY9R_wCBPY58BE5s-kvMHUSslH0nrOy3p5Q76Rju_U5ykeCGYIRfWFxcozZs2A3m7LMZKGYoTU93wpi-OGmOyfBTf8uDhjP-J22YveexSmipK5oEb-93GbQYl5e5p1Uy94p8WEVNedrQ=s500-pd-e365-rw-pc0xffffff',
+    carbs: 14,
+    calories: 180,
+    organic: false,
+    vegan: false,
+    glutenFree: false,
+  },
+  {
+    id: '10',
+    allowed: true,
+    name: 'Natural Creamy Peanut Butter',
+    brand: 'Jif',
+    imageURL: 'https://lh3.googleusercontent.com/proxy/IDdPfanKFWEhR9DpH-vadsrf3Wzfm-sHVL31ey7iT_SWSPdedSgpx3yabjtf2Hvm5aPcgbpXZtJzNAxiSDDCf-2jrbzyMNDg29YYjSpQ0HMgKuBu6qfMwCZbS8_pT7Kr5nNrFzwZoXV07JE5Ooqz3TggK60mXU99IIA-gM8_Li-2hhQVJHFXe4TXL6oHEJrRNL5eJyVpjjLqpdDNl-E=s500-pd-e365-rw-pc0xffffff',
+    carbs: 8,
+    calories: 190,
+    organic: true,
     vegan: true,
     glutenFree: true,
   }
@@ -25,9 +61,8 @@ const dummyDatabase = [
 
 const checkAllowed = scannedItem => {
   let itemData = dummyDatabase.find(item => {
-    item.id === scannedItem.id
+    return item.id === scannedItem.id;
   });
-  // let itemData = dummyDatabase[0];
   itemData.allowed = true;
 
   if (scannedItem.maxCarbs && scannedItem.maxCarbs < itemData.carbs) {
@@ -46,6 +81,7 @@ const checkAllowed = scannedItem => {
 
 app.post('/scan', (req, res) => {
   let item = req.body;
+  console.log(item);
   let response = checkAllowed(item);
   res.end(JSON.stringify(response));
 });
