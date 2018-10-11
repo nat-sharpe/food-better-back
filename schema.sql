9ea7eaeaaac7e22b54d9766bdbb3222dcef0c834
Nat Sharpe <sharpe1890@gmail.com>
2018-10-10 23:58:37 -0400

    Sets up DB

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE items (
+  id INTEGER,
+  allowed VARCHAR,
+  title VARCHAR,
+  brand VARCHAR,
+  imageURL VARCHAR,
+  carbs INTEGER,
+  calories INTEGER,
+  organic VARCHAR,
+  vegan VARCHAR,
+  glutenFree VARCHAR
+);
