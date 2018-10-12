d3b22e85af9842db789c33da6806bbee5b87bbf3
Nat Sharpe <sharpe1890@gmail.com>
2018-10-12 11:20:50 -0400

    Updates seed data

--- seed.sql
+++ seed.sql
@@ -1,2 +1,8 @@
 INSERT INTO items (allowed, title, brand, imageURL, carbs, calories, organic, vegan, glutenFree)
 VALUES ('1', 'true', 'Creamy Peanut Butter', 'Peter Pan', 'https://images-na.ssl-images-amazon.com/images/I/61wP-m6ehjL._SY550_.jpg', 16, 220, 'false', 'false', 'true');
+INSERT INTO items (allowed, title, brand, imageURL, carbs, calories, organic, vegan, glutenFree)
+VALUES ('2', 'true', 'Reduced Fat Crunchy Peanut Butter', 'Jif', 'https://images-na.ssl-images-amazon.com/images/I/715dWDRHw-L._SY450_.jpg', 6, 70, 'false', 'true', 'true');
+INSERT INTO items (allowed, title, brand, imageURL, carbs, calories, organic, vegan, glutenFree)
+VALUES ('20', 'true', 'Reduced Fat Creamy Peanut Butter', 'Skippy', 'https://images-na.ssl-images-amazon.com/images/I/816nVBGE0qL._SL1500_.jpg', 14, 180, 'false', 'false', 'false');
+INSERT INTO items (allowed, title, brand, imageURL, carbs, calories, organic, vegan, glutenFree)
+VALUES ('1', 'true', 'Natural Creamy Peanut Butter', 'Jif', 'https://images-na.ssl-images-amazon.com/images/I/91i5AplQqkL._SL1500_.jpg', 8, 160, 'true', 'true', 'true');
