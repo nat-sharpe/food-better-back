cfbc252b8e6efafc1d9788650e8f6fc923755525
Nat Sharpe <sharpe1890@gmail.com>
2018-10-12 13:00:44 -0400

    Adds database query  to server

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 const pg = require("pg-promise")();
-const DBConfig = `postgres://ubuntu:tesla@foodbetter.fun:3000/foodbetter-db`;
+const DBConfig = "postgres://localhost:5432/ubuntu";
 const DB = pg(DBConfig);
 
-module.exports =  {DB};
+module.exports = DB;
--- server.js
+++ server.js
@@ -1,89 +1,100 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-// const db = require('./db');
+const db = require('./database');
 const dotenv = require('dotenv');
 dotenv.config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const dummyDatabase = [
-  {
-    id: '1',
-    allowed: true,
-    name: 'Creamy Peanut Butter',
-    brand: 'Peter Pan',
-    imageURL: 'https://images-na.ssl-images-amazon.com/images/I/61wP-m6ehjL._SY550_.jpg',
-    carbs: 16,
-    calories: 220,
-    organic: false,
-    vegan: false,
-    glutenFree: true,
-  },
-  {
-    id: '2',
-    allowed: true,
-    name: 'Reduced Fat Crunchy Peanut Butter',
-    brand: 'Jif',
-    imageURL: 'https://images-na.ssl-images-amazon.com/images/I/715dWDRHw-L._SY450_.jpg',
-    carbs: 6,
-    calories: 70,
-    organic: false,
-    vegan: true,
-    glutenFree: true,
-  },
-  {
-    id: '20',
-    allowed: true,
-    name: 'Reduced Fat Creamy Peanut Butter',
-    brand: 'Skippy',
-    imageURL: 'https://images-na.ssl-images-amazon.com/images/I/816nVBGE0qL._SL1500_.jpg',
-    carbs: 14,
-    calories: 180,
-    organic: false,
-    vegan: false,
-    glutenFree: false,
-  },
-  {
-    id: '10',
-    allowed: true,
-    name: 'Natural Creamy Peanut Butter',
-    brand: 'Jif',
-    imageURL: 'https://images-na.ssl-images-amazon.com/images/I/91i5AplQqkL._SL1500_.jpg',
-    carbs: 8,
-    calories: 190,
-    organic: true,
-    vegan: true,
-    glutenFree: true,
-  }
-];
+// const dummyDatabase = [
+//   {
+//     id: '1',
+//     allowed: true,
+//     name: 'Creamy Peanut Butter',
+//     brand: 'Peter Pan',
+//     imageURL: 'https://images-na.ssl-images-amazon.com/images/I/61wP-m6ehjL._SY550_.jpg',
+//     carbs: 16,
+//     calories: 220,
+//     organic: false,
+//     vegan: false,
+//     glutenFree: true,
+//   },
+//   {
+//     id: '2',
+//     allowed: true,
+//     name: 'Reduced Fat Crunchy Peanut Butter',
+//     brand: 'Jif',
+//     imageURL: 'https://images-na.ssl-images-amazon.com/images/I/715dWDRHw-L._SY450_.jpg',
+//     carbs: 6,
+//     calories: 70,
+//     organic: false,
+//     vegan: true,
+//     glutenFree: true,
+//   },
+//   {
+//     id: '20',
+//     allowed: true,
+//     name: 'Reduced Fat Creamy Peanut Butter',
+//     brand: 'Skippy',
+//     imageURL: 'https://images-na.ssl-images-amazon.com/images/I/816nVBGE0qL._SL1500_.jpg',
+//     carbs: 14,
+//     calories: 180,
+//     organic: false,
+//     vegan: false,
+//     glutenFree: false,
+//   },
+//   {
+//     id: '10',
+//     allowed: true,
+//     name: 'Natural Creamy Peanut Butter',
+//     brand: 'Jif',
+//     imageURL: 'https://images-na.ssl-images-amazon.com/images/I/91i5AplQqkL._SL1500_.jpg',
+//     carbs: 8,
+//     calories: 190,
+//     organic: true,
+//     vegan: true,
+//     glutenFree: true,
+//   }
+// ];
 
-const checkAllowed = scannedItem => {
-  let itemData = dummyDatabase.find(item => {
-    return item.id === scannedItem.id;
-  });
-  itemData.allowed = true;
+// const checkAllowed = scannedItem => {
+  // let itemData = dummyDatabase.find(item => {
+  //   return item.id === scannedItem.id;
+  // });
 
-  if (scannedItem.maxCarbs && scannedItem.maxCarbs < itemData.carbs) {
-    itemData.allowed = false;
-  } else if (scannedItem.maxCalories && scannedItem.maxCalories < itemData.calories) {
-    itemData.allowed = false;
-  } else if ((scannedItem.organic === true) && (itemData.organic === false)) {
-    itemData.allowed = false;
-  } else if ((scannedItem.vegan === true) && (itemData.vegan === false)) {
-    itemData.allowed = false;
-  } else if ((scannedItem.glutenFree === true) && (itemData.glutenFree === false)) {
-    itemData.allowed = false;
-  }
-  return itemData;
-}
+  // if (scannedItem.maxCarbs && scannedItem.maxCarbs < itemData.carbs) {
+  //   itemData.allowed = false;
+  // } else if (scannedItem.maxCalories && scannedItem.maxCalories < itemData.calories) {
+  //   itemData.allowed = false;
+  // } else if ((scannedItem.organic === true) && (itemData.organic === false)) {
+  //   itemData.allowed = false;
+  // } else if ((scannedItem.vegan === true) && (itemData.vegan === false)) {
+  //   itemData.allowed = false;
+  // } else if ((scannedItem.glutenFree === true) && (itemData.glutenFree === false)) {
+  //   itemData.allowed = false;
+  // }
+  // return itemData;
+// }
 
 app.post('/scan', (req, res) => {
   let item = req.body;
   console.log(item);
-  let response = checkAllowed(item);
-  res.end(JSON.stringify(response));
+  // let response = checkAllowed(item);
+  db.one(`SELECT * FROM items WHERE id = ${item.id};`)
+      .then(function (data) {
+        res.status(200)
+          .json({
+            status: 'success',
+            data: data,
+            message: 'Retrieved ALL puppies'
+          });
+      })
+      .catch(function (err) {
+        return next(err);
+      });
+  // res.end(JSON.stringify(response));
 });
 
 
